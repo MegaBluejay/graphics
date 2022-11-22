ffa53d8533333c761bc23b951f878e8f1dec4190
MegaBluejay <dmoiseev2011@gmail.com>
2022-11-22 23:19:00 +0300

    add rect drawing

---
+++ graphics/draw.py
@@ -0,0 +1,44 @@
+from itertools import chain
+from math import ceil, floor
+
+import numpy as np
+
+from .utils import blockwise_view, pairwise
+
+
+def inter(y, p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return x1 + (x2 - x1) * (y - y1) / (y2 - y1)
+
+
+def draw_rect(im_shape, ps):
+    im_shape = np.array(im_shape)
+    big_im = np.zeros(im_shape * 4)
+    ps = ps * 4 - 2
+    lines = list(pairwise(chain(ps, ps[:1])))
+    pi, (_, miny) = min(enumerate(ps), key=lambda q: q[1][1])
+    li1, li2 = (pi - 1) % 4, pi
+    y = ceil(miny)
+    sides = 0
+    while True:
+        if y > lines[li1][0][1]:
+            li1 = (li1 - 1) % 4
+            sides += 1
+        if y > lines[li2][1][1]:
+            li2 = (li2 + 1) % 4
+            sides += 1
+        if sides == 4:
+            return blockwise_view(big_im, (4, 4)).sum(axis=(2, 3)) / 16
+        x1, x2 = inter(y, *lines[li1]), inter(y, *lines[li2])
+        big_im[y, ceil(x1) : floor(x2)] = 1
+        y += 1
+
+
+def draw_line(im_shape, p1, p2, width):
+    h = p2 - p1
+    w_half = width * np.array([[0, 1], [-1, 0]]) @ h / np.linalg.norm(h)
+    ps = np.array([p1 + w_half, p2 + w_half, p2 - w_half, p1 - w_half])
+    for i in range(2):
+        np.clip(ps[:, i], 0, im_shape[i] - 1, ps[:, i])
+    return draw_rect(im_shape, ps)
--- graphics/utils.py
+++ graphics/utils.py
@@ -1,4 +1,7 @@
+from itertools import tee
+
 import numpy as np
+from numpy.lib.stride_tricks import as_strided
 
 
 def normalize(image, max_val):
@@ -7,3 +10,17 @@ def normalize(image, max_val):
 
 def to_8bit(image):
     return np.round(image * 255).astype("u1")
+
+
+def pairwise(it):
+    a, b = tee(iter(it))
+    next(b, None)
+    return zip(a, b)
+
+
+def blockwise_view(a, block_shape):
+    outer_shape = tuple(np.array(a.shape) // block_shape)
+    view_shape = outer_shape + block_shape
+    inner_strides = a.strides
+    outer_strides = tuple(a.strides * np.array(block_shape))
+    return as_strided(a, shape=view_shape, strides=outer_strides + inner_strides)
